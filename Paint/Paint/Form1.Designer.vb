b48429ae18e25d1344f99c3663dbaaa7f16d88b1
ToxicHawk <31133607+ToxicHawk@users.noreply.github.com>
2017-08-18 09:44:59 -0400

    Makes Paint boi trollz and lots o colors
    Im bad at coding ;P

---
+++ Paint/Paint/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(57, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(0, 25)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(142, 37)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "Select A Color"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(148, 25)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(131, 37)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Reset Pen Size"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(872, 457)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Paint/Paint/Form1.vb
@@ -0,0 +1,77 @@
+﻿Public Class Form1
+    Dim mouse As Boolean = False
+    Dim draw As System.Drawing.Graphics
+    Dim PxPos As Integer
+    Dim PyPos As Integer
+    Dim xPos As Integer
+    Dim yPos As Integer
+    Dim radius As Integer = 3
+    Dim MyBrush As New SolidBrush(Color.Black)
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+
+
+
+
+
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        Dim draw As System.Drawing.Graphics
+        draw = Me.CreateGraphics
+
+        PxPos = xPos
+        PyPos = yPos
+
+        xPos = Me.MousePosition.X - Me.Location.X - 15
+        yPos = Me.MousePosition.Y - Me.Location.Y - 40
+
+
+        Label1.Text = Me.MousePosition.X
+
+        Label2.Text = Me.MousePosition.Y
+
+        Dim mouseDown As Integer = MouseButtons
+
+        If mouse = True Then
+            draw.DrawLine(New Pen(MyBrush, radius), PxPos, PyPos, xPos, yPos)
+        End If
+    End Sub
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+        mouse = True
+    End Sub
+
+    Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles Me.MouseUp
+        mouse = False
+    End Sub
+
+    Private Sub Form1_MouseWheel(sender As Object, e As MouseEventArgs) Handles Me.MouseWheel
+        radius += 1
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        MyBrush.Color = Color.Red
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        MyBrush.Color = Color.LawnGreen
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+        MyBrush.Color = Color.SpringGreen
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ColorDialog1.ShowDialog()
+        MyBrush.Color = ColorDialog1.Color
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        radius = 3
+    End Sub
+End Class
